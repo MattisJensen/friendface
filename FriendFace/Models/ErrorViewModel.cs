dd3edab09dca56eed028779217c6a85a69de74de
Bertram Petersen <tilbertrampetersen@gmail.com>
2023-09-27 10:41:19 +0200

    the stuff

---
+++ FriendFace/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FriendFace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
