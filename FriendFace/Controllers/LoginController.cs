3a7f75ade667d782765fe39d30d3cc86cf327edf
looseMole <62012079+looseMole@users.noreply.github.com>
2023-10-24 21:39:25 +0200

    "Fix" login method - now returns boolean

--- FriendFace/Controllers/LoginController.cs
+++ FriendFace/Controllers/LoginController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Npgsql;
 using Microsoft.Extensions.Configuration;
+using NpgsqlTypes;
 
 namespace FriendFace.Controllers
 {
@@ -10,9 +11,8 @@ namespace FriendFace.Controllers
             .AddUserSecrets<Program>()
             .Build();
         
-        private static string _connString = _config["connString"]; // Retrieves the connection string as a stored secret, on the format: "jdbc:postgresql://cornelius.db.elephantsql.com:5432/<database>"
+        private static string _connString = _config["connString"]; // Retrieves the connection string as a stored secret, on the format: "Host=cornelius.db.elephantsql.com:5432;Username=<username>;Password=<password;Database=<database>"
         
-
         
         // Main login-page
         public IActionResult Index()
@@ -20,6 +20,35 @@ namespace FriendFace.Controllers
             return View();
         }
         
+        // Handling the log-in info
+        public async Task<bool> DoLogin()
+        {
+            // Establish DB connection
+            await using var dataSource = NpgsqlDataSource.Create(_connString);
+            
+            // Sanitize input
+            // ...
+            
+            string username = Request.Form["uname"];
+            string password = Request.Form["psw"]; // This far, the password is probably plain-text.
+            
+            bool hasReturn;
+            
+            // Insert input data
+            await using (var cmd = dataSource.CreateCommand("SELECT id FROM users WHERE username = $1 " +
+                                                            "AND password = crypt($2, password)"))
+            {
+                cmd.Parameters.AddWithValue(NpgsqlDbType.Varchar, username);
+                cmd.Parameters.AddWithValue(NpgsqlDbType.Varchar, password);
+
+                await using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    hasReturn = reader.HasRows;
+                }
+            }
+
+            return hasReturn;
+        }
         
         // Registration-page
         public async Task<bool> Register()
