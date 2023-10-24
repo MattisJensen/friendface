c576299008e6a3b9f78e422509f7b83b6fc69602
looseMole <62012079+looseMole@users.noreply.github.com>
2023-10-24 21:41:50 +0200

    Create Registration functionality
    Thus far, like the DoLogin() method, the DoRegister method returns a boolean, as a result of the expression's result or failure.

--- FriendFace/Controllers/LoginController.cs
+++ FriendFace/Controllers/LoginController.cs
@@ -20,6 +20,12 @@ namespace FriendFace.Controllers
             return View();
         }
         
+        // Register new user page
+        public IActionResult Register()
+        {
+            return View();
+        }
+        
         // Handling the log-in info
         public async Task<bool> DoLogin()
         {
@@ -51,19 +57,46 @@ namespace FriendFace.Controllers
         }
         
         // Registration-page
-        public async Task<bool> Register()
+        public async Task<bool> DoRegister()
         {
             // Establish DB connection
-            await using var dataSource = NpgsqlDataSource.Create(_connString); // Possibly have to call a different method, for login credentials.
+            await using var dataSource = NpgsqlDataSource.Create(_connString);
+            
+            string username = Request.Form["uname"];
+            string password = Request.Form["psw"]; // This far, the password is probably plain-text.
+            string email = Request.Form["email"];
+
+            bool affectedOneRow;
             
             // Sanitize input
             // ...
             
+            
             // Insert input data
-            await using (var cmd = dataSource.CreateCommand("INSERT INTO "))
+            await using (var cmd = dataSource.CreateCommand("INSERT INTO users (username, email, password) " +
+                                                            "VALUES ($1, $2, crypt($3, gen_salt('bf')))")) // Do some database "sanitation" here. Think of what will happen if user already exists, for instance. 
             {
+                cmd.Parameters.AddWithValue(NpgsqlDbType.Varchar, username);
+                cmd.Parameters.AddWithValue(NpgsqlDbType.Varchar, email);
+                cmd.Parameters.AddWithValue(NpgsqlDbType.Varchar, password);
                 
+                await cmd.ExecuteNonQueryAsync();
             }
+            
+            // Verify insertion
+            await using (var cmd = dataSource.CreateCommand("SELECT id FROM users WHERE username = $1 " +
+                                                            "AND password = crypt($2, password)"))
+            {
+                cmd.Parameters.AddWithValue(NpgsqlDbType.Varchar, username);
+                cmd.Parameters.AddWithValue(NpgsqlDbType.Varchar, password);
+            
+                await using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    affectedOneRow = reader.HasRows;;
+                }
+            }
+
+            return affectedOneRow;
         }
     }
 }
