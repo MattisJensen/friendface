c3c22f934e8c81aea9069fbfaf1afa42f01cb671
MattisJensen <115666021+MattisJensen@users.noreply.github.com>
2023-10-31 22:14:50 +0100

    Added basic logic for posts in user feed

--- FriendFace/Controllers/HomeController.cs
+++ FriendFace/Controllers/HomeController.cs
@@ -1,21 +1,43 @@
 ﻿using FriendFace.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using FriendFace.Data;
+using FriendFace.ViewModels;
 using Microsoft.Extensions.Logging;
+using System.Linq;
 
 namespace FriendFace.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            /*User userLoggedIn = _context.Users.Find(1); // !! here we still need to find the user that is logged in, and handle if no user is logged in !!
+
+            // Fetches the latest posts from the users that the logged in user follows
+            var latestPostsFromFollowing = (from post in _context.Posts
+                where userLoggedIn.FollowedUsers.Any(follow => follow.FollowedUserId == post.User.Id)
+                orderby post.Time descending
+                select post).ToList();
+
+
+            HomeIndexViewModel homeIndexViewModel = new HomeIndexViewModel()
+            {
+                User = userLoggedIn,
+                PostsInFeed = latestPostsFromFollowing
+            };
+
+            return View(homeIndexViewModel);*/
+
             return View();
         }
 
---
+++ FriendFace/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,10 @@
+using FriendFace.Models;
+
+namespace FriendFace.ViewModels;
+
+public class HomeIndexViewModel
+{
+    // contains all the information needed for the home page
+    public User User { get; set; }
+    public List<Post> PostsInFeed { get; set; }
+}
