a81ce7f33c6c4d38aa74cdd9daf1d74cb771a5d4
MattisJensen <115666021+MattisJensen@users.noreply.github.com>
2023-10-31 23:36:20 +0100

    Fixed DBContext IdentityUser in latest ASP .net EF

--- FriendFace/Data/ApplicationDbContext.cs
+++ FriendFace/Data/ApplicationDbContext.cs
@@ -1,13 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FriendFace.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace FriendFace.Data
-
 {
-    using FriendFace.Models;
-    using Microsoft.AspNet.Identity.EntityFramework;
-    using System.Data.Entity;
-
-    public class ApplicationDbContext : IdentityDbContext<User>
+    public class ApplicationDbContext : IdentityDbContext
     {
         // DbSet for each entity/table we want to interact with
         public DbSet<User> Users { get; set; }
@@ -16,51 +13,44 @@ namespace FriendFace.Data
         public DbSet<UserLikesPost> UserLikesPosts { get; set; }
         public DbSet<Comment> Comments { get; set; }
 
-        public ApplicationDbContext() : base("DefaultConnection", throwIfV1Schema: false)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
         }
 
-        public static ApplicationDbContext Create()
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            return new ApplicationDbContext();
+            base.OnModelCreating(modelBuilder); // This needs to be called before the custom model configurations
+
+            /*   // User to Post relation: One-to-Many
+               modelBuilder.Entity<User>()
+                   .HasMany(u => u.Posts)
+                   .WithRequired(p => p.User)
+                   .HasForeignKey(p => p.UserId);
+
+               // User to Comment relation: One-to-Many
+               modelBuilder.Entity<User>()
+                   .HasMany(u => u.Comments)
+                   .WithRequired(c => c.User)
+                   .HasForeignKey(c => c.UserId);
+
+               // Post to Comment relation: One-to-Many
+               modelBuilder.Entity<Post>()
+                   .HasMany(p => p.Comments)
+                   .WithRequired(c => c.Post)
+                   .HasForeignKey(c => c.PostId);
+
+               // Follower-Following relation: Many-to-Many
+               modelBuilder.Entity<User>()
+                   .HasMany(u => u.Following)
+                   .WithMany(u => u.Followers)
+                   .Map(cs =>
+                   {
+                       cs.MapLeftKey("UserId");
+                       cs.MapRightKey("FollowerId");
+                       cs.ToTable("UserFollowers");
+                   });
+           }*/
         }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            /* base.OnModelCreating(modelBuilder); // This needs to be called before the custom model configurations
-
-            // User to Post relation: One-to-Many
-            modelBuilder.Entity<User>()
-                .HasMany(u => u.Posts)
-                .WithRequired(p => p.User)
-                .HasForeignKey(p => p.UserId);
-
-            // User to Comment relation: One-to-Many
-            modelBuilder.Entity<User>()
-                .HasMany(u => u.Comments)
-                .WithRequired(c => c.User)
-                .HasForeignKey(c => c.UserId);
-
-            // Post to Comment relation: One-to-Many
-            modelBuilder.Entity<Post>()
-                .HasMany(p => p.Comments)
-                .WithRequired(c => c.Post)
-                .HasForeignKey(c => c.PostId);
-
-            // Follower-Following relation: Many-to-Many
-            modelBuilder.Entity<User>()
-                .HasMany(u => u.Following)
-                .WithMany(u => u.Followers)
-                .Map(cs =>
-                {
-                    cs.MapLeftKey("UserId");
-                    cs.MapRightKey("FollowerId");
-                    cs.ToTable("UserFollowers");
-                });
-        }*/
     }
-}
-
 }
--- FriendFace/Program.cs
+++ FriendFace/Program.cs
@@ -12,6 +12,8 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
