cb579bb2588787c4e4ed0717f1167629fd0b9369
phped22 <phped22@student.sdu.dk>
2023-11-08 00:16:02 +0100

    Added view & viewmodels for profile edit & view

---
+++ FriendFace/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,20 @@
+﻿
+
+public class EditProfileViewModel
+
+{
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Email { get; set; }
+
+    public string Bio { get; set; }
+
+    // This will be used just to display the existing profile picture
+    public string ProfilePicturePath { get; set; }
+
+    // This will be used to upload a new profile picture
+    // public HttpPostedFileBase ProfilePicture { get; set; }
+}
