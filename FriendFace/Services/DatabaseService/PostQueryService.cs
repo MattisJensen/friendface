f354924c62d8601d83dcf4f58085eba4d5773ade
MattisJensen <115666021+MattisJensen@users.noreply.github.com>
2023-11-07 21:59:35 +0100

    Show likes on post

--- FriendFace/Services/DatabaseService/PostQueryService.cs
+++ FriendFace/Services/DatabaseService/PostQueryService.cs
@@ -6,7 +6,30 @@ namespace FriendFace.Services.DatabaseService;
 
 public class PostQueryService
 {
-    public static List<Post> getLatestPostsFromFollowingUserIDs(ApplicationDbContext context, List<int> followingUserIds)
+    public static Post getPostFromId(ApplicationDbContext context, int postId)
+    {
+        return context.Posts
+            .Include(p => p.User)
+            .Include(p => p.Likes)
+            .Include(p => p.Comments)
+            .FirstOrDefault(p => p.Id == postId);
+    }
+
+    public static int getNumberOfLikes(ApplicationDbContext context, int postId)
+    {
+        return context.UserLikesPosts
+            .Count(ul => ul.PostId == postId);
+    }
+
+    public static bool userHasLikedPost(ApplicationDbContext context, int userId, int postId)
+    {
+        // Checks UserLikesPost for the given user and post IDs
+        return context.UserLikesPosts
+            .Any(ulp => ulp.UserId == userId && ulp.PostId == postId);
+    }
+
+    public static List<Post> getLatestPostsFromFollowingUserIDs(ApplicationDbContext context,
+        List<int> followingUserIds)
     {
         return context.Posts
             .Include(p => p.User)
