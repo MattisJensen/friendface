52b8d8b4ecafb0553c695f0550bf774d4dcbece6
looseMole <62012079+looseMole@users.noreply.github.com>
2023-11-07 21:15:06 +0100

    Create DeletePost method

--- FriendFace/Controllers/PostController.cs
+++ FriendFace/Controllers/PostController.cs
@@ -55,4 +55,17 @@ public class PostController : Controller
 
         return true;
     }
+
+    public bool DeletePost(int postId)
+    {
+        if (_context.Posts.Find(postId) == null)
+        {
+            throw new KeyNotFoundException();
+        }
+
+        _context.Posts.Remove(_context.Posts.Find(postId));
+        _context.SaveChanges();
+        
+        return true;
+    }
 }
