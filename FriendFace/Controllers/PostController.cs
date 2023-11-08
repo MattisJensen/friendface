36ba1fd786cafbe288cb8a1fdf2da75dba0e3647
looseMole <62012079+looseMole@users.noreply.github.com>
2023-11-08 11:37:52 +0100

    Create PostController methods for service methods

--- FriendFace/Controllers/PostController.cs
+++ FriendFace/Controllers/PostController.cs
@@ -1,6 +1,8 @@
 using FriendFace.Data;
 using FriendFace.Models;
+using FriendFace.Services.DatabaseService;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileSystemGlobbing.Internal.PatternContexts;
 
 namespace FriendFace.Controllers;
 
@@ -15,5 +17,50 @@ public class PostController : Controller
     public PostController(ApplicationDbContext context)
     {
         this._context = context;
+    }
+
+    // Queries
+    public Post GetPostFromId(ApplicationDbContext context, int postId)
+    {
+        return PostQueryService.getPostFromId(context, postId);
+    }
+
+    public int GetNumberOfLikes(ApplicationDbContext context, int postId)
+    {
+        return PostQueryService.getNumberOfLikes(context, postId);
+    }
+
+    public bool UserHasLikedPost(ApplicationDbContext context, int userId, int postId)
+    {
+        return PostQueryService.userHasLikedPost(context, userId, postId);
+    }
+
+    public List<Post> GetLatestPostsFromFollowingUserIDs(ApplicationDbContext context,
+        List<int> followingUserIds)
+    {
+        return PostQueryService.getLatestPostsFromFollowingUserIDs(context, followingUserIds);
+    }
+    
+    // Creation
+    public bool CreatePost(ApplicationDbContext context, string content, User sourceUser)
+    {
+        return PostCreateService.CreatePost(context, content, sourceUser);
+    }
+    
+    
+    // Updates
+    public void AddLikeToPost(ApplicationDbContext context, int postId, int userId)
+    {
+        PostUpdateService.addLikeToPost(context, postId, userId);
+    }
+
+    public bool UpdatePost(ApplicationDbContext context, int postId, string updatedContent)
+    {
+        return PostUpdateService.UpdatePost(context, postId, updatedContent);
+    }
+
+    public bool DeletePost(ApplicationDbContext context, int postId)
+    {
+        return PostUpdateService.DeletePost(context, postId);
     }
 }
