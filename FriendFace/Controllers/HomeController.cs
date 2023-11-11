6a860473b282b97e23e713cae61a32d28b8ddc4c
MattisJensen <115666021+MattisJensen@users.noreply.github.com>
2023-11-11 21:05:05 +0100

    Added toggle Like on Post functionality

--- FriendFace/Controllers/HomeController.cs
+++ FriendFace/Controllers/HomeController.cs
@@ -25,11 +25,11 @@ namespace FriendFace.Controllers
         {
             // !! here we still need to find the user that is logged in, and handle if no user is logged in !!
             User loggedInUser = UserQueryService.getUser(_context, 1);
-            
             HomeIndexViewModel homeIndexViewModel = new HomeIndexViewModel()
             {
                 User = loggedInUser,
-                PostsInFeed = PostQueryService.getLatestPostsFromFollowingUserIDs(_context, UserQueryService.getFollowingUserIds(loggedInUser))
+                PostsInFeed = PostQueryService.getLatestPostsFromFollowingUserIDs(_context,
+                    UserQueryService.getFollowingUserIds(loggedInUser))
             };
 
             return View(homeIndexViewModel);
@@ -45,10 +45,62 @@ namespace FriendFace.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-        
-        public void likePost(int userId)
+
+        [HttpPost]
+        public async Task<IActionResult> LikePost([FromBody] int postId)
         {
-            
+            var post = await _context.Posts.FindAsync(postId);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            var user = UserQueryService.getUser(_context, 1);
+
+            var existingLike =
+                await _context.UserLikesPosts.SingleOrDefaultAsync(like =>
+                    like.UserId == user.Id && like.PostId == postId);
+
+            if (existingLike != null)
+            {
+                // If a like by the user already exists, remove it
+                _context.UserLikesPosts.Remove(existingLike);
+            }
+            else
+            {
+                // If no like by the user exists, add a new one
+                var like = new UserLikesPost
+                {
+                    UserId = user.Id,
+                    PostId = postId
+                };
+
+                _context.UserLikesPosts.Add(like);
+            }
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPostLikes([FromQuery] int postId)
+        {
+            var post = PostQueryService.getPostFromId(_context, postId);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            var user = UserQueryService.getUser(_context, 1);
+
+            var isLiked = post.Likes.Any(like => like.UserId == user.Id);
+
+            var likeCount = post.Likes.Count;
+
+            return Json(new { likeCount = likeCount, isLiked = isLiked });
         }
     }
 }
---
+++ FriendFace/wwwroot/js/Home/toggleLikeOfPost.js
@@ -0,0 +1,27 @@
+async function toggleLikeOfPost(heartIcon) {
+    var postId = heartIcon.getAttribute('data-post-id');
+    console.log('postId:', postId);
+
+    try {
+        const response = await fetch('/Home/LikePost', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(postId)
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to like/unlike post');
+        }
+
+        const dataResponse = await fetch(`/Home/GetPostLikes?postId=${postId}`);
+        const data = await dataResponse.json();
+
+        document.getElementById(`likeCount-${postId}`).textContent = data.likeCount;
+        heartIcon.className = data.isLiked ? "fas fa-heart" : "far fa-heart";
+        heartIcon.style.color = data.isLiked ? "red" : "inherit";
+    } catch (error) {
+        console.error('There was an error:', error);
+    }
+}
