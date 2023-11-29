5f909a5c918540a4d0adbfe52430b5b8da4ca5ee
Bertram Højlund Petersen <bepet22@student.sdu.dk>
2023-11-29 14:11:31 +0100

    New Migrations

--- FriendFace/Migrations/20231129130251_removeignore.Designer.cs
+++ FriendFace/Migrations/20231129130251_removeignore.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FriendFace.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FriendFace.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231129130251_removeignore")]
+    partial class removeignore
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
